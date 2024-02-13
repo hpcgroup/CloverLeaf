8d589b7e8ce1e335f9cbf703807e427b4c93f42a
Joy Kitson <jkitson@umd.edu>
2024-02-13 07:46:36 -0800

    Revert "Removed post-reduce  copy from OpenACC version"
    This reverts commit c7571f6324d9a7e2ca2960bc87f4e888105a3e28.

--- src/acc/field_summary.cpp
+++ src/acc/field_summary.cpp
@@ -118,6 +118,19 @@ void field_summary(global_variables &globals, parallel_ &parallel) {
       ke += cell_mass * 0.5 * vsqrd;
       press += cell_vol * pressure[j + (k)*base_stride];
     }
+    
+    if (globals.profiler_on) {
+      globals.profiler.summary += timer() - kernel_time;
+      kernel_time = timer();
+    }
+
+#pragma acc exit data copyout(vol, mass, ie, ke, press)
+
+    if (globals.profiler_on) {
+      globals.profiler.device_to_host += timer() - kernel_time;
+      kernel_time = timer();
+    }
+  }
 
 #if SYNC_BUFFERS
   globals.deviceToHost();
